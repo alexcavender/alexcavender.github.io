c80c3d75526eff5ae01e06d3dbb9dbbc52270cb2
Alex Cavender <alexcavender@gmail.com>
2016-04-10 23:29:52 -0400

    add js file

---
+++ _site/js/main.js
@@ -0,0 +1,23 @@
+function hidePopUp() {
+   $('.popup').css('opacity', '0');
+   $('.popup').css('visibility', 'hidden');
+}
+
+function showPopUp() {
+  $('.popup').css('opacity', '1');
+  $('.popup').css('visibility', 'visible');
+}
+
+$('.contact-me').click(function() {
+  showPopUp();
+});
+
+
+$('.popup').click(function(event) {      
+  if ( $( event.target).is('.email-me') || $( event.target).is('.name') || $( event.target).is('.email') || $( event.target).is('.message') || $( event.target).is('.send') ) {
+    event.stopPropagation();
+  }
+  else {
+    hidePopUp();
+  }
+});
