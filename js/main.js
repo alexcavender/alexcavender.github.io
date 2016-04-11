16d121f6be5ce4f0658628d7e0eab539ddeca0bc
Alex Cavender <alexcavender@gmail.com>
2016-04-10 23:33:21 -0400

    add load jquery

--- js/main.js
+++ js/main.js
@@ -1,23 +1,27 @@
-function hidePopUp() {
-   $('.popup').css('opacity', '0');
-   $('.popup').css('visibility', 'hidden');
-}
+$( document ).ready(function() {
 
-function showPopUp() {
-  $('.popup').css('opacity', '1');
-  $('.popup').css('visibility', 'visible');
-}
-
-$('.contact-me').click(function() {
-  showPopUp();
-});
-
-
-$('.popup').click(function(event) {      
-  if ( $( event.target).is('.email-me') || $( event.target).is('.name') || $( event.target).is('.email') || $( event.target).is('.message') || $( event.target).is('.send') ) {
-    event.stopPropagation();
+  function hidePopUp() {
+     $('.popup').css('opacity', '0');
+     $('.popup').css('visibility', 'hidden');
   }
-  else {
-    hidePopUp();
+
+  function showPopUp() {
+    $('.popup').css('opacity', '1');
+    $('.popup').css('visibility', 'visible');
   }
+
+  $('.contact-me').click(function() {
+    showPopUp();
+  });
+
+
+  $('.popup').click(function(event) {      
+    if ( $( event.target).is('.email-me') || $( event.target).is('.name') || $( event.target).is('.email') || $( event.target).is('.message') || $( event.target).is('.send') ) {
+      event.stopPropagation();
+    }
+    else {
+      hidePopUp();
+    }
+  });
+
 });
